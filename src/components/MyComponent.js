14cb5af997187e4108f56cb68b9cb0bd14256932
Cậu Bé Vuôi Vẻ <92206104+nhonnam@users.noreply.github.com>
2022-11-12 21:34:03 +0700

    properties

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,19 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
   //JSX
   render() {
+    const myInfor = ["ab", "c", "c"];
     return (
       <div>
         <UserInfor />
+        <br />
+        <br />
+        <DisplayInfor name="NhonNam" age="20" />
+        <hr />
+        <DisplayInfor name="CauBeZuiZe" age={20} myInfor={myInfor} />
       </div>
     );
   }
