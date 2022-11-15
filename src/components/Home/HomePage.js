b53c519f0610c5f6486716ca2bc30da71778dc1c
Cậu Bé Vuôi Vẻ <92206104+nhonnam@users.noreply.github.com>
2022-11-15 09:50:16 +0700

    design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,13 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = (props) => {
-  return <div className="homepage-container">home page component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
